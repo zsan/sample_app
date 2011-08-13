d12c34f40ab19f95e514dadb4dcf48380ceb816a
zsan <zaksantanu@gmail.com>
2011-08-14 04:27:51 +0700

    done with chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,7 @@ module ApplicationHelper
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
 	if @title.nil?
-	  #base_title
-
-	  "#{base_title} | #{@title}";
+	  base_title
 	else
 	  "#{base_title} | #{@title}";
 	end
